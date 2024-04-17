0b8de7cc827503a21bcbcaac9f69841735ced402
muhammadzaeemaltaf <165184980+muhammadzaeemaltaf@users.noreply.github.com>
2024-04-18 00:08:26 +0500

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# TypeScript_Currency_Converter
+
+CLI tool for currency conversion. Input source, target currencies, and amount for accurate exchange rate calculation. Converts currencies based on provided rates.
---
+++ index.js
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const currency = {
+    USD: 1,
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 280
+};
+const currencyChoices = Object.keys(currency);
+let userAnswer = await inquirer.prompt([
+    {
+        name: "from",
+        message: "Enter From Currency",
+        type: "list",
+        choices: currencyChoices,
+    },
+    {
+        name: "to",
+        message: "Enter To Currency",
+        type: "list",
+        choices: currencyChoices,
+    },
+    {
+        name: "amount",
+        message: "Enter Your Amount",
+        type: "number",
+    }
+]);
+let fromAmount = currency[userAnswer.from];
+let toAmount = currency[userAnswer.to];
+let amount = userAnswer.amount;
+let convertedAmount = (amount / fromAmount) * toAmount;
+console.log(convertedAmount.toFixed(2));
